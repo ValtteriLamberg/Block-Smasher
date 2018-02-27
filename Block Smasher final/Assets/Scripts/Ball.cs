36e518826c7bef977f471af1c8a3e9bcde5df78b
ValtteriLamberg <36888680+ValtteriLamberg@users.noreply.github.com>
2018-02-27 15:28:31 +0200

    Lol
    My first game!

---
+++ Block Smasher final/Assets/Scripts/Ball.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    private Paddle paddle;
+
+    private bool hasStarted = false;
+
+    private Vector3 paddleToBallVector;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        paddle = GameObject.FindObjectOfType<Paddle>();
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+        print(paddleToBallVector.y);
+
+        audioSource = GetComponent<AudioSource>();
+        if(audioSource.playOnAwake)
+        {
+            audioSource.playOnAwake = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+
+            // Lock the ball relative to the paddle.
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+
+            //Wait for a mouse press to launch.
+            if (Input.GetMouseButtonDown(0))
+            {
+                hasStarted = true;
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+            }
+        }
+
+		
+	}
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        Vector2 tweak = new Vector2(Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+
+        if(hasStarted)
+        {
+            audioSource.Play();
+   
+            GetComponent<Rigidbody2D>().velocity += tweak;
+        }
+       
+    }
+}
---
+++ Block Smasher final/Assets/Scripts/Paddle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    public float minPos, maxPos;
+    
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		/*
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
+
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, -7.5f, 7.5f);
+
+        this.transform.position = paddlePos;
+		*/
+
+
+		Vector3 mousepos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+
+		transform.position = new Vector2 (Mathf.Clamp(mousepos.x,minPos, maxPos), transform.position.y);
+
+
+    }
+}
